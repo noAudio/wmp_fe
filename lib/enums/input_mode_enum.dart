e9b9c87cc6ded3b41eb422368716e29d1675ab0c
Samuel Odongo <samieodd@gmail.com>
2023-03-29 05:13:57 +0300

    Created enum for input modes in the edit modal

---
+++ lib/enums/input_mode_enum.dart
@@ -0,0 +1,13 @@
+/// Defines input modes for the input elements
+/// in the editing modal.
+///
+/// `InputMode.search` refers to the text input that
+/// allows the user to filter foods based on a key
+/// word.
+///
+/// `InputMode.text` refers to the text input that
+/// allows the user to set a name for a new food.
+///
+/// `InputMode.number` refers to the number input that
+/// allows the user to set a price for a new food.
+enum InputMode { search, text, number }
